590650bd0367f7a39360696662d5e0fe22afc410
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2020-06-10 20:18:27 -0400

    Add files via upload

--- source/src/menu.cpp
+++ source/src/menu.cpp
@@ -1,5 +1,6 @@
 #include "menu.h"
 #include "menu_item.h"
+#include "input.h"
 #include "globals.h"
 
 Menu::Menu()
@@ -10,24 +11,25 @@ Menu::Menu()
 Menu::Menu(Graphics &graphics)
 {
     //Populate _menuItems with MenuItem objects
-    /*
-    this->_menuItems.emplace_back("save", graphics, 69, 3, 16, 16, Vector2(100, 100) );
-    this->_menuItems.emplace_back("music", graphics, 69, 21, 16, 16, Vector2(100, 100) );
-    this->_menuItems.emplace_back("sfx", graphics, 69, 39, 16, 16, Vector2(100, 100) );
-    */
+    this->_menuItems.emplace_back("save", graphics, 3, 3, 16, 16, Vector2( 32, globals::GAME_VIEWPORT_H + (globals::MENU_VIEWPORT_H - 64 )));
+    this->_menuItems.emplace_back("music", graphics, 3, 21, 16, 16, Vector2( 32 + (40 * 1) , globals::GAME_VIEWPORT_H + (globals::MENU_VIEWPORT_H - 64 )) );
+    this->_menuItems.emplace_back("sfx", graphics, 3, 39, 16, 16, Vector2( 32 + (40 * 2), globals::GAME_VIEWPORT_H + (globals::MENU_VIEWPORT_H - 64 )) );
 
-    this->_menuItems.push_back( MenuItem("save", graphics, 46, 3, 16, 16, Vector2( 32, globals::GAME_VIEWPORT_H + (globals::MENU_VIEWPORT_H - 64 ))) );
-    this->_menuItems.push_back( MenuItem("music", graphics, 46, 21, 16, 16, Vector2( 32 + (40 * 1) , globals::GAME_VIEWPORT_H + (globals::MENU_VIEWPORT_H - 64 ))) );
-    this->_menuItems.push_back( MenuItem("sfx", graphics, 46, 39, 16, 16, Vector2( 32 + (40 * 2), globals::GAME_VIEWPORT_H + (globals::MENU_VIEWPORT_H - 64))) );
-    this->_menuItems.push_back( MenuItem("bulletCard", graphics, 3, 3, 40, 40, Vector2(200, globals::GAME_VIEWPORT_H)) );
-
+    //Populate _towerMenuItems with TowerMenuItems
+    this->_towerMenuItems.emplace_back("bulletTower", graphics, 58, 3, 40, 40, Vector2(200, globals::GAME_VIEWPORT_H));
+    this->_towerMenuItems.emplace_back("rocketTower", graphics, 58, 45, 40, 40, Vector2(300, globals::GAME_VIEWPORT_H));
 }
 
-void Menu::update(int elapsedTime)
+void Menu::update(int elapsedTime, Input &input)
 {
     for(int i = 0; i < this->_menuItems.size(); ++i)
     {
-        this->_menuItems.at(i).update(elapsedTime);
+        this->_menuItems.at(i).update(elapsedTime, input);
+    }
+
+      for(int i = 0; i < this->_towerMenuItems.size(); ++i)
+    {
+        this->_towerMenuItems.at(i).update(elapsedTime, input);
     }
 }
 
@@ -39,4 +41,32 @@ void Menu::draw(Graphics &graphics)
         int y = this->_menuItems.at(i).getY();
         this->_menuItems.at(i).draw(graphics, x, y);
     }
+
+    for(int i = 0; i < this->_towerMenuItems.size(); ++i)
+    {
+        int x = this->_towerMenuItems.at(i).getX();
+        int y = this->_towerMenuItems.at(i).getY();
+        this->_towerMenuItems.at(i).draw(graphics, x, y);
+    }
+}
+
+Tower* Menu::getTower(Graphics &graphics, int mouseX, int mouseY)
+{
+    Tower* tower = nullptr;
+
+    for(int i = 0; i < this->_towerMenuItems.size(); ++i)
+    {
+        if(this->_towerMenuItems.at(i).isClicked())
+        {
+            tower = this->_towerMenuItems.at(i).createTower(graphics, mouseX, mouseY);
+
+            if(tower != nullptr)
+            {
+                return tower;
+            }
+        }
+    }
+
+    //Memory leak if I don't delete tower?
+    return nullptr;
 }
--- source/src/menu_item.cpp
+++ source/src/menu_item.cpp
@@ -1,5 +1,8 @@
 #include "menu_item.h"
+#include "rectangle.h"
 #include "graphics.h"
+#include "input.h"
+#include "tower.h"
 
 MenuItem::MenuItem()
 {
@@ -8,13 +11,51 @@ MenuItem::MenuItem()
 
 MenuItem::MenuItem(std::string name, Graphics &graphics, int sourceX, int sourceY, int width, int height, Vector2 spawnPoint)
     :   Sprite(graphics, "content/sprites/menu.png", sourceX, sourceY, width, height, spawnPoint.x, spawnPoint.y),
-        _name(name)
+        _name(name),
+        _clicked(false)
 {
 
 }
 
-void MenuItem::update(int elapsedTime)
+void MenuItem::update(int elapsedTime, Input &input)
 {
+    int mouseX, mouseY;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    Rectangle mousePosition = {mouseX, mouseY, 0, 0};
+
+    //The music and sfx icons should toggle on or off, but the save icon should
+    //only have the pressed-down sprite while the left mouse button is held down
+    if( mousePosition.collidesWith(this->getBoundingBox()) )
+    {
+        if(!this->_clicked)
+        {
+             this->onHover();
+        }
+
+        if(this->_name == "save" && input.getLeftClick())
+        {
+            this->onClick();
+        }
+
+        if( (this->_name == "music" || this->_name == "sfx") && input.getLeftReleased() )
+        {
+            this->_clicked = !this->_clicked;
+            this->onClick();
+        }
+    }
+    else
+    {
+        if(this->_name == "save")
+        {
+            setSprite(this->_name, 3, this->_sourceRect.y, 16, 16);
+        }
+
+        if(( this->_name == "music" || this->_name == "sfx") && !this->_clicked)
+        {
+            setSprite(this->_name, 3, this->_sourceRect.y, 16, 16);
+        }
+    }
+
     Sprite::update();
 }
 
@@ -28,7 +69,101 @@ void MenuItem::draw(Graphics &graphics, int x, int y)
     graphics.blitMenu(this->_spriteSheet, &this->_sourceRect, &destRectangle);
 }
 
-void MenuItem::setSprite(int sourceX, int sourceY, int width, int height)
+void MenuItem::setSprite(std::string name, int sourceX, int sourceY, int width, int height)
+{
+    this->_name = name;
+    this->_sourceRect.x = sourceX;
+    this->_sourceRect.y = sourceY;
+    this->_sourceRect.w = width;
+    this->_sourceRect.h = height;
+}
+
+void MenuItem::onHover()
+{
+    setSprite(this->_name, 21, this->_sourceRect.y, 16, 16);
+}
+
+void MenuItem::onClick()
+{
+    //The music and sfx icons should toggle on and off, while the save icon should only
+    //stay on the the third sprite as long as the left mouse button is held down on it.
+    if( (this->_name == "music" || this->_name == "sfx") )
+    {
+        if(this->_clicked)
+        {
+            setSprite(this->_name, 39, this->_sourceRect.y, 16, 16);
+        }
+        else
+        {
+            setSprite(this->_name, 21, this->_sourceRect.y, 16, 16);
+        }
+    }
+    else if(this->_name == "save")
+    {
+        setSprite(this->_name, 39, this->_sourceRect.y, 16, 16);
+    }
+}
+
+//TowerMenuItem class
+
+TowerMenuItem::TowerMenuItem()
 {
 
+}
+
+TowerMenuItem::TowerMenuItem(std::string name, Graphics &graphics, int sourceX, int sourceY, int width, int height, Vector2 spawnPoint)
+    :   MenuItem(name, graphics, sourceX, sourceY, width, height, spawnPoint)
+{
+
+}
+
+void TowerMenuItem::update(int elapsedTime, Input &input)
+{
+    this->_clicked = false;
+
+    int mouseX, mouseY;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    Rectangle mousePosition = {mouseX, mouseY, 0, 0};
+
+    if( mousePosition.collidesWith(this->getBoundingBox()) )
+    {
+        this->onHover();
+
+        if(input.getLeftReleased())
+        {
+            this->_clicked = true;
+        }
+    }
+    else
+    {
+        setSprite(this->_name, 58, this->_sourceRect.y, 40, 40);
+    }
+
+    Sprite::update();
+}
+
+void TowerMenuItem::onHover()
+{
+    setSprite(this->_name, 100, this->_sourceRect.y, 40, 40);
+}
+
+Tower* TowerMenuItem::createTower(Graphics &graphics, int mouseX, int mouseY)
+{
+    Tower* tower = nullptr;
+
+    if(this->_name == "bulletTower")
+    {
+       tower = new BulletTower(graphics, Vector2(mouseX, mouseY));
+    }
+    else if(this->_name == "rocketTower")
+    {
+       tower = new RocketTower(graphics, Vector2(mouseX, mouseY));
+    }
+
+    if(tower != nullptr)
+    {
+        tower->setDragged(true);
+    }
+
+    return tower;
 }
